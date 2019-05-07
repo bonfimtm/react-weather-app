c2fec498d2d8e1e15bea2589b19a1d15497ea627
Thiago Bonfim <bonfimtm@gmail.com>
2019-05-07 09:32:57 +0200

    Add docs folder

---
+++ docs/precache-manifest.c5e0a9f687bc035adefe9608f54dbcdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9420d78fb0d897407b06b208dda0107",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "1f52994afc0f9dc825c5",
+    "url": "/react-weather-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e2215ddd4461f041d19b",
+    "url": "/react-weather-app/static/css/main.56626bf0.chunk.css"
+  },
+  {
+    "revision": "1f52994afc0f9dc825c5",
+    "url": "/react-weather-app/static/js/2.f464af3b.chunk.js"
+  },
+  {
+    "revision": "e2215ddd4461f041d19b",
+    "url": "/react-weather-app/static/js/main.bd1b06e6.chunk.js"
+  },
+  {
+    "revision": "e736fc5812ad96aa92b3",
+    "url": "/react-weather-app/static/js/runtime~main.7e96bc80.js"
+  }
+]);
